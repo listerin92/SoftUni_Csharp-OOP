7fb7d0cd5ded1e644d2a159c6064cad0b862f91c
Dobromir Petrov <luzterin@gmail.com>
2020-07-02 22:11:21 +0300

    final

---
+++ Encapsulation/Persons/CommonValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class CommonValidator
+    {
+        public static void ValidateNull(object value, string typeName, string propertyName)
+        {
+            if (value == null)
+            {
+                throw new NullReferenceException($"{typeName}.{propertyName} can not be null");
+            }
+        }
+
+        public static void ValidateMinimum<T>(T value, T minimum, string typeName, string propertyName) where T : IComparable<T>
+        {
+            if (value.CompareTo(minimum) < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{typeName}.{propertyName} cannot contain fewer than {minimum} symbols!");
+            }
+        }
+
+        public static void ValidateLength(string text, int minimumLength, string typeName, string propertyName)
+        {
+            ValidateNull(text, typeName, propertyName);
+            ValidateMinimum(text.Length, minimumLength, typeName, propertyName);
+        }
+    }
+}
---
+++ Encapsulation/Persons/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using static PersonsInfo.CommonValidator;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        private const int MinimumNameLength = 3;
+        private const int MinimumSalary = 460;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            private set
+            {
+                ValidateLength(value, MinimumNameLength, nameof(Person), nameof(this.FirstName));
+
+                this.firstName = value;
+
+            }
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+
+            private set
+            {
+                ValidateLength(value, MinimumNameLength, nameof(Person), nameof(this.LastName));
+                
+                this.lastName = value;
+
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+
+            private set
+            {
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => this.salary;
+            private set
+            {
+                ValidateNull(value, nameof(Person), nameof(this.Salary));
+                ValidateMinimum(value, MinimumSalary, nameof(Person), nameof(this.Salary));
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            SetSalary(percentage, this.Age > 30 ? 100 : 200);
+        }
+
+        private void SetSalary(decimal percentage, int ageMod)
+        {
+            this.Salary += this.Salary * percentage / ageMod;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
