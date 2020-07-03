1caf0e2827862c3807bace1473e58c71b24c38fe
Dobromir Petrov <luzterin@gmail.com>
2020-07-03 17:49:02 +0300

    final 100/100

---
+++ EncapsulationExercise/ShoppingSpree/Person.cs
@@ -0,0 +1,59 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public IReadOnlyList<Product> BagOfProducts => this.bagOfProducts;
+        
+        public decimal Money
+        {
+            get => this.money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public void Add(Product product)
+        {
+            this.bagOfProducts.Add(product);
+        }
+        //public override string ToString()
+        //{
+        //    StringBuilder stringBuilder = new StringBuilder();
+        //    stringBuilder.Append($"Name: {this.Name}, Money: {this.Money}");
+
+        //    return stringBuilder.ToString();
+        //}
+
+    }
+}
